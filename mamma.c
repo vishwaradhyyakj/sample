55cade6c90c90b4a16a21df009a053f8e9a9e9d5
vishwaradhyyakj <vishwakj0305@gmail.com>
2024-07-03 11:19:23 +0530

    Add files via upload

---
+++ mamma.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+// Function to perform bubble sort
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n-1; i++) {
+        // Last i elements are already in place
+        for (j = 0; j < n-i-1; j++) {
+            // Swap if the element found is greater than the next element
+            if (arr[j] > arr[j+1]) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    printf("Original array: \n");
+    printArray(arr, n);
+    
+    bubbleSort(arr, n);
+    
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    
+    return 0;
+}
